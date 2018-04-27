b9a03fe316cd518765cf478423537b1a130cc242
Kevin Butler <kb@honeypot.io>
2018-04-27 10:23:19 +0200

    Add an index to the login within the actor json payload

--- githubarchive.rb
+++ githubarchive.rb
@@ -88,6 +88,7 @@ class Githubarchive
         DateTime :inserted_at, null: false # Utility column
         DateTime :updated_at, null: false # Utility column
       end
+      database.run("CREATE INDEX actor_login ON events ((actor->>'login'));")
     end
 
     def drop_table(name = :events)
